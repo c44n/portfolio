181cc09f01a080b8c9de4f1fa6b4292a72c600f1
Can <cantpts@outlook.de>
2026-03-31 10:05:23 +0200

    feat: add header component, style header

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,11 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from './layout/header/header';
 import { Footer } from './layout/footer/footer';
+import { Hero } from './layout/hero/hero';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header, Footer],
+  imports: [RouterOutlet, Header, Hero, Footer],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
