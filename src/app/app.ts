312f27b2737da4b44f11e203eec545224fcf228c
Can <cantpts@outlook.de>
2026-04-02 09:58:23 +0200

    feat: add technologies component and start with card-box design

--- src/app/app.ts
+++ src/app/app.ts
@@ -4,10 +4,11 @@ import { Header } from './layout/header/header';
 import { Footer } from './layout/footer/footer';
 import { Hero } from './layout/hero/hero';
 import { About } from './layout/about/about';
+import { Technologies } from './layout/technologies/technologies';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header, Hero, About, Footer],
+  imports: [RouterOutlet, Header, Hero, About, Technologies, Footer],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
---
+++ src/app/layout/technologies/technologies.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-technologies',
+  imports: [],
+  templateUrl: './technologies.html',
+  styleUrl: './technologies.scss',
+})
+export class Technologies {}
