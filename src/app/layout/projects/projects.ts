ae9193de2ce0d1828bed9b6b6e0a3bd8623e89e2
Can <cantpts@outlook.de>
2026-04-13 07:12:33 +0200

    feat: install syncfusion carousel component

--- src/app/layout/projects/projects.ts
+++ src/app/layout/projects/projects.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { CarouselModule } from '@syncfusion/ej2-angular-navigations';
 
 @Component({
   selector: 'app-projects',
-  imports: [],
+  imports: [CarouselModule],
   templateUrl: './projects.html',
   styleUrl: './projects.scss',
 })
