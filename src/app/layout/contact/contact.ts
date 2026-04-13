0bfc848611bae87ed8a23747536b982a91de0b4f
Can <cantpts@outlook.de>
2026-04-13 08:46:58 +0200

    feat: contact section and form styling

--- src/app/layout/contact/contact.ts
+++ src/app/layout/contact/contact.ts
@@ -1,9 +1,22 @@
 import { Component } from '@angular/core';
+import { FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
-  selector: 'app-contact',
-  imports: [],
-  templateUrl: './contact.html',
-  styleUrl: './contact.scss',
+	selector: 'app-contact',
+	imports: [ReactiveFormsModule],
+	templateUrl: './contact.html',
+	styleUrl: './contact.scss',
 })
-export class Contact {}
+export class Contact {
+	firstname = new FormControl('', {
+		validators: [Validators.required, Validators.minLength(3)]
+	});
+
+	email = new FormControl('', {
+		validators: [Validators.required, Validators.minLength(8)]
+	});
+
+	desc = new FormControl('', {
+		validators: [Validators.required, Validators.minLength(3)]
+	});
+}
