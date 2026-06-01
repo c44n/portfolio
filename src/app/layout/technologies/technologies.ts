6730fb665320e6a7c0c7418544cdaa306d9f9c48
Can <cantpts@outlook.de>
2026-06-01 18:56:26 +0200

    feat: technologies tech add overlay

--- src/app/layout/technologies/technologies.ts
+++ src/app/layout/technologies/technologies.ts
@@ -1,9 +1,22 @@
+import { OverlayModule, ConnectedPosition } from '@angular/cdk/overlay';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-technologies',
-  imports: [],
+  imports: [OverlayModule],
   templateUrl: './technologies.html',
   styleUrl: './technologies.scss',
 })
-export class Technologies {}
+export class Technologies {
+  isOpen = false;
+
+  readonly overlayPositions: ConnectedPosition[] = [
+    {
+      originX: 'center',
+      originY: 'top',
+      overlayX: 'center',
+      overlayY: 'bottom',
+      offsetY: -10,    
+    }
+  ]
+}
